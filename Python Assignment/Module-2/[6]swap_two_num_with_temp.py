249242780c2d91a82125e0969698a226319bca3a
tejashchaudhari <109734065+tejashchaudhari@users.noreply.github.com>
2022-11-23 15:50:57 +0530

    Add files via upload

---
+++ Python Assignment/Module-2/[6]swap_two_num_with_temp.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of A:"))
+b=int(input("Enter the value of B:"))
+temp=a
+a=b
+b=temp
+print("Value of after swap A is:",a)
+print("Value of after swap B is:",b)
---
+++ Python Assignment/Module-2/[7]evenorodd.py
@@ -0,0 +1,5 @@
+num=int(input("Enet the number are chek the even or odd:"))
+if num%2==0:
+    print("The Number is even.!")
+else:
+    print("The number is odd..!")
