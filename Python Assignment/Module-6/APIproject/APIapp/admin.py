221dd31414d6d225a4b0582c6109d9b79f949b89
tejashchaudhari <109734065+tejashchaudhari@users.noreply.github.com>
2023-04-06 02:22:15 +0530

    Add files via upload

---
+++ Python Assignment/Module-6/APIproject/APIapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Book_model
+
+# Register your models here.
+
+
+class Book_admin (admin.ModelAdmin):
+
+    list_display = ['Title','Author','Isbn','Publisher']
+
+admin.site.register(Book_model,Book_admin)
+
